345f6a094f110c9711b166066040f48454980c12
Webners1 <Webners1>
2021-11-10 18:37:03 +0500

    gs1 exchange

---
+++ src/views/Home/components/CakeDataRow.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
+import { getBalanceNumber, formatLocalisedCompactNumber } from 'utils/formatBalance'
+import { usePriceCakeBusd } from 'state/farms/hooks'
+import { Flex, Text, Heading, Skeleton } from '@pancakeswap/uikit'
+import { useTranslation } from 'contexts/Localization'
+import Balance from 'components/Balance'
+import tokens from 'config/constants/tokens'
+
+const StyledColumn = styled(Flex)<{ noMobileBorder?: boolean }>`
+  flex-direction: column;
+  ${({ noMobileBorder, theme }) =>
+    noMobileBorder
+      ? `${theme.mediaQueries.md} {
+           padding: 0 16px;
+           border-left: 1px ${theme.colors.inputSecondary} solid;
+         }
+       `
+      : `border-left: 1px ${theme.colors.inputSecondary} solid;
+         padding: 0 8px;
+         ${theme.mediaQueries.sm} {
+           padding: 0 16px;
+         }
+       `}
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-gap: 16px 8px;
+  margin-top: 24px;
+  grid-template-columns: repeat(2, auto);
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    grid-gap: 16px;
+  }
+
+  ${({ theme }) => theme.mediaQueries.md} {
+    grid-gap: 32px;
+    grid-template-columns: repeat(4, auto);
+  }
+`
+
+const emissionsPerBlock = 14.5
+
+const CakeDataRow = () => {
+  const { t } = useTranslation()
+  const totalSupply = useTotalSupply()
+  const burnedBalance = getBalanceNumber(useBurnedBalance(tokens.cake.address))
+  const cakeSupply = totalSupply ? getBalanceNumber(totalSupply) - burnedBalance : 0
+  const cakePriceBusd = usePriceCakeBusd()
+  const mcap = cakePriceBusd.times(cakeSupply)
+  const mcapString = formatLocalisedCompactNumber(mcap.toNumber())
+
+  return (
+    <Grid>
+      <div>.</div>
+    </Grid>
+  )
+}
+
+export default CakeDataRow
---
+++ src/views/Home/components/SunburstSvg.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Svg, SvgProps } from '@pancakeswap/uikit'
+
+const SunburstSvg: React.FC<SvgProps> = (props) => {
+  return (
+    <Svg viewBox="0 0 1956 1956" {...props}>
+      .
+    </Svg>
+  )
+}
+
+export default SunburstSvg
